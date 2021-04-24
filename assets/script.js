66a7ab999773157f9f0c74be319bb1fcdd73531b
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-04-23 23:11:30 -0400

    working on the java script file, so far buildling the questionBank

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+
+
+var questionBank = [{
+    questionIs: 'Commonly used data types DO NOT include:',
+    ansOne: '1. strings',
+    ansTwo: '2. booleans',
+    ansThree: '3. alerts',
+    ansFour: '4. numbers',
+    correctAns: '3',
+}, {
+    questionIs: 'The condition in an if / else statement is enclosed wtihin ________.',
+    ansOne: '1. quotes',
+    ansTwo: '2. curly brackets',
+    ansThree: '3. parantheses',
+    ansFour: '4. square brackets',
+    correctAns: '3',
+}, {
+    
+}
+]
+
+console.log(questionBank);
