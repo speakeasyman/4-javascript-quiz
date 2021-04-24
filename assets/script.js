d94212aeb29d806d57946b996b7141d3cda3af65
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-04-24 18:43:40 -0400

    got the logic to check an answer working

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ var btnOne = document.getElementById('btnOne');
 var btnTwo = document.getElementById('btnTwo');
 var btnThree = document.getElementById('btnThree');
 var btnFour = document.getElementById('btnFour');
+var btnFive = document.getElementById('btnFive');
 var btnAns = document.getElementsByClassName('questionBtn');
-var answer = '';
+var theAnswer = '';
 var questionNumber = 0;
 
 
@@ -16,35 +17,35 @@ var questionBank = [{
     ansTwo: '2. booleans',
     ansThree: '3. alerts',
     ansFour: '4. numbers',
-    correctAns: '3',
+    correctAns: 'btnThree',
 }, {
     questionIs: 'The condition in an if / else statement is enclosed wtihin ________.',
     ansOne: '1. quotes',
     ansTwo: '2. curly brackets',
     ansThree: '3. parantheses',
     ansFour: '4. square brackets',
-    correctAns: '3',
+    correctAns: 'btnThree',
 }, {
     questionIs: 'Arrays in JavaScript can be used to store __.',
     ansOne: '1. numbers and strings',
     ansTwo: '2. other arrays',
     ansThree: '3. booleans',
     ansFour: '4. all of the above',
-    correctAns: '4',
+    correctAns: 'btnFour',
 }, {
     questionIs: 'String values must be enclosed wtihin ____ when being assigned to variables.',
     ansOne: '1. commas',
     ansTwo: '2. curly brackets',
     ansThree: '3. quotes',
     ansFour: '4. parentheses',
-    correctAns: '3',
+    correctAns: 'btnThree',
 }, {
     questionIs: 'A very useful tool used during development and debugging for printing content to the debugger is:',
     ansOne: '1. JavaScript',
     ansTwo: '2. terminal / bash',
     ansThree: '3. for loops',
     ansFour: '4. console log',
-    correctAns: '4',
+    correctAns: 'btnFour',
 }]
 
 startBtn.addEventListener('click', function() {
@@ -67,6 +68,8 @@ function showbutton() {
     btnTwo.style.display = 'flex';
     btnThree.style.display = 'flex';
     btnFour.style.display = 'flex';
+    btnFive.style.display = 'none';
+
 }
 
 function questionMake() {
@@ -76,27 +79,37 @@ function questionMake() {
         btnTwo.textContent = questionBank[questionNumber].ansTwo;
         btnThree.textContent = questionBank[questionNumber].ansThree;
         btnFour.textContent = questionBank[questionNumber].ansFour;
+        btnFive.textContent = questionBank[questionNumber].ansFive;
     }
      }
 
 function questionCheck() {
-    document.getElementById('btnOne').onclick = answer;
-    document.getElementById('btnTwo').onclick = answer;
-    document.getElementById('btnThree').onclick = answer;
-    document.getElementById('btnFour').onclick = answer;
-    function answer(clicked) {
-        console.log(this.id);
+    
+    if (questionBank[questionNumber].correctAns == answer) {
+        console.log('first half' + btnFive.value);
+        console.log('second half ' + answer);
+        console.log(`this is correct`);
+    } else {
+        console.log(`WRONG`);
+        console.log('first half' + questionBank[questionNumber].correctAns);
+        console.log('second half ' + answer);
     }
     
-
-
-
-
+    
+    
+    
+    
+    
     questionNumber++;
     
     
-
+    
     questionMake();
-
+    
 }
 
+
+function choice(clicked) {
+       answer = clicked
+    console.log(clicked);
+}
