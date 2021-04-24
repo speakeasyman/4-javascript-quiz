636ab91400a137706d52914071b29d21cc690fdf
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-04-23 23:58:16 -0400

    finished question bank, added showbutton function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+var startBtn = document.getElementById('startBtn');
+var btnOne = document.getElementById('btnOne');
+var btnTwo = document.getElementById('btnTwo');
+var btnThree = document.getElementById('btnThree');
+var btnFour = document.getElementById('btnFour');
+
 
 
 var questionBank = [{
@@ -15,8 +21,40 @@ var questionBank = [{
     ansFour: '4. square brackets',
     correctAns: '3',
 }, {
+    questionIs: 'Arrays in JavaScript can be used to store __.',
+    ansOne: '1. numbers and strings',
+    ansTwo: '2. other arrays',
+    ansThree: '3. booleans',
+    ansFour: '4. all of the above',
+    correctAns: '4',
+}, {
+    questionIs: 'String values must be enclosed wtihin ____ when being assigned to variables.',
+    ansOne: '1. commas',
+    ansTwo: '2. curly brackets',
+    ansThree: '3. quotes',
+    ansFour: '4. parentheses',
+    correctAns: '3',
+}, {
+    questionIs: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    ansOne: '1. JavaScript',
+    ansTwo: '2. terminal / bash',
+    ansThree: '3. for loops',
+    ansFour: '4. console log',
+    correctAns: '4',
+}]
+
+startBtn.addEventListener('click', function() {
+    showbutton();
+})
+
+function showbutton() {
+    startBtn.style.display = 'none';
+    btnOne.style.display = 'flex';
+    btnTwo.style.display = 'flex';
+    btnThree.style.display = 'flex';
+    btnFour.style.display = 'flex';
+}
+
+function questionBank() {
     
 }
-]
-
-console.log(questionBank);
